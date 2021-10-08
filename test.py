bdfc309981a0bf0f085a4557c16806b679198f81
HenryNS <80058112+HenryNS@users.noreply.github.com>
2021-10-08 13:55:30 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from pipython import GCSDevice,pitools
-import serial.tools.list_ports
-from pipython.gcserror import GCSError
-import sys
-import numpy as np;
-import libtiepie
-from libtiepie.examples.printinfo import *
